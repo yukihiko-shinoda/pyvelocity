eb48930b68a46bbe6acfc9efe11f0cba5bb731da
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2022-09-18 14:19:23 +0900

    Make setup.py empty

--- setup.py
+++ setup.py
@@ -3,9 +3,4 @@
 
 from setuptools import setup  # type: ignore
 
-setup(
-    # pipenv-setup causes error without dependency_links property:
-    # https://github.com/Madoshakalaka/pipenv-setup/issues/23
-    dependency_links=[],
-    install_requires=["click>=7.0", "tomli", "typing-extensions"],
-)
+setup()
